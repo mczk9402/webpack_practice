e09d8265ab3775c74950e1a8b8f7e174a04318a7
mczk9402 <47809450+mczk9402@users.noreply.github.com>
2021-01-07 23:25:20 +0900

    name-hash test

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: './javascripts/main.js'
+    filename: './javascripts/[name]-[hash].js'
   },
   module: {
     rules: [
@@ -19,8 +19,8 @@ module.exports = {
         use: [
           {
             loader: 'ts-loader',
-          }
-        ]
+          },
+        ],
       },
       {
         test: /\.js/,
@@ -61,7 +61,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name: 'images/[name].[ext]',
+              name: 'images/[name]-[hash].[ext]',
               publicPath: '/',
             },
           },
@@ -71,8 +71,8 @@ module.exports = {
               mozjpeg: {
                 progressiive: true,
                 quality: 65,
-              }
-            }
+              },
+            },
           },
         ],
       },
@@ -94,7 +94,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './stylesheets/main.css',
+      filename: './stylesheets/[name]-[hash].css',
     }),
     new HtmlWebpackPlugin({
       template: './src/templates/index.pug',
